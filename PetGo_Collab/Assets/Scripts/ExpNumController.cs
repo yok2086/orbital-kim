eed46892716b973e5a64d0436ab55de8f1afc1e3
hyunjinkim1112 <hyun.jin.kim.1112@gmail.com>
2024-06-30 08:49:29 +0900

    changed ui slider controller

--- PetGo_Collab/Assets/Scripts/ExpNumController.cs
+++ PetGo_Collab/Assets/Scripts/ExpNumController.cs
@@ -7,16 +7,31 @@ using TMPro;
 public class ExpNumController : MonoBehaviour
 {
     public Slider slider; 
-    [SerializeField] private TextMeshProUGUI levelText = null;
+    public float sliderVal; 
+    private float level; 
+    public float levelVal; 
+    private float expNum = 0.0f; 
 
-    //public int level; 
+    [SerializeField] private TextMeshProUGUI levelText = null;
+    [SerializeField] private TextMeshProUGUI sliderText = null;
+    [SerializeField] private float maxSliderAmount = 200.0f;
+
+
     //public int sample; 
 
     void Start() 
     {
-        slider.value = StaticData.sliderValue; //slider = GameObject.Find("XpBar").GetComponent<Slider>();
-        levelText.text = StaticData.levelValue.ToString();//level = 0; 
-        
+        // slider.value = StaticData.sliderValue; //slider = GameObject.Find("XpBar").GetComponent<Slider>();
+        // levelText.text = StaticData.levelValue.ToString();//level = 0; 
+        slider.value = PlayerPrefs.GetFloat("sliderVal", 0);
+        levelText.text = PlayerPrefs.GetFloat("levelVal", 0).ToString();
+    }
+
+    public void SliderChange (float value)
+    {
+        float expNum = value * maxSliderAmount;
+        sliderText.text = expNum.ToString("0");  
+        Debug.Log("slider value =" + expNum);
     }
 
 
@@ -24,31 +39,49 @@ public class ExpNumController : MonoBehaviour
     void Update()
     {
         
-        if (StaticData.hungryBar >= 1.0f) {
+        // if (StaticData.hungryBar >= 1.0f) {
+        //     slider.value += 0.5f;
+        //     StaticData.hungryBar = 0.0f;
+        // }
+        // if (StaticData.cleanBar >= 1.0f) {
+        //     slider.value += 0.05f;
+        //     StaticData.cleanBar = 0.0f; 
+        // }
+        // if (StaticData.sleepBar >= 1.0f) {
+        //     slider.value += 0.05f;
+        //     StaticData.sleepBar = 0.0f; 
+        // }
+        if (PlayerPrefs.GetFloat("HungryBar", 0)>= 1.0f) {
             slider.value += 0.5f;
-            StaticData.hungryBar = 0.0f;
+            PlayerPrefs.SetFloat("HungryBar", 0);
         }
-        if (StaticData.cleanBar >= 1.0f) {
+        if (PlayerPrefs.GetFloat("CleanBar", 0) >= 1.0f) {
             slider.value += 0.05f;
-            StaticData.cleanBar = 0.0f; 
+            PlayerPrefs.SetFloat("CleanBar", 0);
         }
-        if (StaticData.sleepBar >= 1.0f) {
+        if (PlayerPrefs.GetFloat("SleepBar", 0) >= 1.0f) {
             slider.value += 0.05f;
-            StaticData.sleepBar = 0.0f; 
+            PlayerPrefs.SetFloat("SleepBar", 0);
         }
 
         if(slider.value >= 1.0f) {
             //level += 1;
-            StaticData.levelValue += 1;
-            levelText.text = StaticData.levelValue.ToString();
-            
+            //StaticData.levelValue += 1;
+            level = PlayerPrefs.GetFloat("levelVal", 0);
+            level += 1;
+            //PlayerPrefs.SetFloat("levelVal", level);
+            //levelText.text = StaticData.levelValue.ToString();
+            levelText.text = level.ToString();
             //StaticData.levelValue = level; 
 
             slider.value = 0.0f;
-            Debug.Log(StaticData.levelValue);
+           // Debug.Log(StaticData.levelValue);
 
         }
-        StaticData.sliderValue = slider.value; 
+        //StaticData.sliderValue = slider.value; 
+        PlayerPrefs.SetFloat("sliderVal", slider.value);
+        PlayerPrefs.SetFloat("levelVal", level);
+
 
         
     }
